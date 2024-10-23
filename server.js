40cdc32da26cd8460746f8058ff213efe47909e2
Mitsiuk Olha <mitsyuk.olga@gmail.com>
2024-10-23 11:28:50 +0100

    fix port

--- server.js
+++ server.js
@@ -7,14 +7,29 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 
-// разрешение забирать статические файлы из папки
-app.use (express.static("public"));
-
 const api = require("./api");
 app.use("/api", api);
 
-// начинаем прослушивать подключение на 3000 порту
+// разрешение забирать статические файлы из папки
+// app.use (express.static("public"));
+if (process.env.NODE_ENV === 'production') {
 
-app.listen(3000, function () {
-    console.log("http://localhost:3000");
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    app.get('*', (req, res) => {
+
+        res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+
+    });
+
+}
+
+// начинаем прослушивать подключение на 3000 порту
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log(`http://localhost:${PORT}`);
 });
+
+// для продакшена
+
